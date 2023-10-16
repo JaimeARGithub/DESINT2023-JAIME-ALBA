dd8086ba8f1de6539d97600dee4604d0e74aec51
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-16 14:40:19 +0200

    16/10/2023
    Ejemplo de uso de Modelo Vista-Controlador sin base de datos. Alguna cosita no funciona, revisar los USING del main.

---
+++ ExampleMVCNoDatabase/ExampleMVCNoDatabase/Domain/People.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleMVCNoDatabase.Persistence.Manages;
+
+namespace ExampleMVCNoDatabase.Domain
+{
+    public class People
+    {
+        private String name;
+        private int age;
+        private PeopleManage pm;
+
+        public People(String nameIntr, int ageIntr)
+        {
+            this.Name = nameIntr;
+            this.Age = ageIntr;
+            Pm = new PeopleManage();
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public PeopleManage Pm { get => pm; set => pm = value; }
+    }
+}
---
+++ ExampleMVCNoDatabase/ExampleMVCNoDatabase/Persistence/Manages/PeopleManage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleMVCNoDatabase.Domain;
+
+namespace ExampleMVCNoDatabase.Persistence.Manages
+{
+    public class PeopleManage
+    {
+        private DataTable table;
+        private List<People> listPeople;
+
+        public PeopleManage()
+        {
+            this.table = new DataTable();
+            ListPeople = new List<People>();
+        }
+
+        public DataTable Table { get => table; set => table = value; }
+        public List<People> ListPeople { get => listPeople; set => listPeople = value; }
+
+        // To insert customers
+        public void readPeople()
+        {
+            ListPeople.Add(new People("Neil", 42));
+            ListPeople.Add(new People("Jimi", 20));
+            ListPeople.Add(new People("Yara", 19));
+        }
+    }
+}
