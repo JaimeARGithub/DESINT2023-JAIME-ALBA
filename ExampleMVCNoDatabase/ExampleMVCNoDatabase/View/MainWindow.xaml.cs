2c3949df1d267b7fa72af7a10e2e06fd0a8a589d
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-23 14:28:32 +0200

    23/10/2023
    Actualización de MVCnoDatabase. Añadido el dejar campos en blanco, el rellenar campos de texto al seleccionar un registro, el eliminar registro, el guardar registro nuevo y el modificar registro antiguo. Habilitar y deshabilitar botones.

--- ExampleMVCNoDatabase/ExampleMVCNoDatabase/View/MainWindow.xaml.cs
+++ ExampleMVCNoDatabase/ExampleMVCNoDatabase/View/MainWindow.xaml.cs
@@ -27,16 +27,94 @@ namespace ExampleMVCnoDatabase
             People people = new People();
             people.readP();
             dgvPeople.ItemsSource = people.getListPeople();
+            // De entrada, nuevo está habilitado para meter registros si quiero
+            // Borrar sólo se habilita si selecciono un registro para borrarlo
+            btnDel.IsEnabled = false;
+            btnNew.IsEnabled = false;
+        }
+
+        private void start()
+        {
+            txtName.Text = "";
+            txtAge.Text = "";
+            btnDel.IsEnabled = false;
+            btnNew.IsEnabled = false;
+
+            dgvPeople.UnselectAll();
         }
 
         private void dgvPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            MessageBox.Show("Selección cambiada");
+            //MessageBox.Show("Selección cambiada");
+            // dgvPeople es un array de objetos que tengo guardados.
+            // dgvPeople.SelectedItems[0] me va a devolver el primero
+            // de los elementos de ese array, y para poder utilizar los
+            // métodos de la clase People tengo que hacerle un casting
+
+            // El nombre de la textbox a la que le quiero ajustar el texto
+            // junto con la propiedad .Text me va a permitir ajustarle el
+            // texto que contiene.
+            if (dgvPeople.SelectedItems.Count>0)
+            {
+                btnNew.IsEnabled = true;
+                btnDel.IsEnabled = true;
+                People p = (People)dgvPeople.SelectedItems[0]; // Si selecciono varios, sólo me pone el primero
+                txtName.Text = p.name;
+                txtAge.Text = p.age.ToString();
+            }
+            
         }
 
-        private void btnExample_Click(object sender, RoutedEventArgs e)
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("ESTOY DESQUICIADO");
+            // Al darle a NEW, los campos se quedan en blanco, el botón de
+            // borrar se deshabilita y se quita la selección
+            start();
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to remove this person?","Confirmation",MessageBoxButton.YesNo)==MessageBoxResult.Yes) {
+                // El objeto que quiero borrar.
+                People p = (People)dgvPeople.SelectedItems[0];
+
+                // Necesito recuperar la list de la DataGrid.
+                List<People> lista = (List<People>) dgvPeople.ItemsSource;
+
+                // Y borro el objeto de la lista.
+                lista.Remove(p);
+
+                // Y hacemos un refresh para que los cambios
+                // se vean reflejados.
+                dgvPeople.Items.Refresh();
+                dgvPeople.ItemsSource = lista;
+
+                // Volvemos al estado inicial.
+                start();
+            }
+        }
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgvPeople.SelectedItems.Count > 0)
+            {
+                btnDel.IsEnabled = true;
+                People p = (People)dgvPeople.SelectedItems[0]; // Si selecciono varios, sólo me pone el primero
+                p.name = txtName.Text;
+                p.age = Int32.Parse(txtAge.Text);
+
+                dgvPeople.Items.Refresh();
+            } else
+            {
+                List<People> lista = (List<People>)dgvPeople.ItemsSource;
+                lista.Add(new People(txtName.Text, Int32.Parse(txtAge.Text)));
+
+                dgvPeople.Items.Refresh();
+                dgvPeople.ItemsSource = lista;
+            }  
+
         }
     }
 }
