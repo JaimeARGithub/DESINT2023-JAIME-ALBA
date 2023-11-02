c80285288b0959d232d1377655c1647b116cce23
jaimealba <jaimealba@outlook.es>
2023-11-02 01:04:51 +0100

    02/11/2023 01:05
    D'Hondt perfeccionado.

---
+++ HijoBastardo/HijoBastardo/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HijoBastardo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPartidos = 10;
+            int escañosMadrid = 37;
+            int habitantesVotaron = 4345230;
+            int votosNulos = habitantesVotaron / 20;
+
+            int votosValidos = habitantesVotaron - votosNulos;
+
+            double[] porcentajes = { 35.25, 24.75, 15.75, 14.25, 3.75, 3.25, 1.5, 0.5, 0.25, 0.25 };
+            for (int i = 0; i < porcentajes.Length; i++)
+            {
+                if (porcentajes[i] < 3)
+                {
+                    porcentajes[i] = 0;
+                }
+            }
+
+
+            int[,] votos = new int[numPartidos, escañosMadrid];
+
+
+            for (int i = 0; i < numPartidos; i++)
+            {
+                votos[i, 0] = (int)((porcentajes[i] * votosValidos) / 100);
+            }
+
+
+
+            for (int i = 0; i < votos.GetLength(0); i++)
+            {
+                for (int j = 1; j < votos.GetLength(1); j++)
+                {
+                    votos[i, j] = votos[i, 0] / (j + 1);
+                }
+            }
+
+
+            imprimir(votos);
+
+
+            int[] partidos = new int[numPartidos];
+
+            int partidoMayor = 0;
+            for (int i = 0; i < escañosMadrid; i++)
+            {
+                partidoMayor = encontrarMayor(votos);
+                partidos[partidoMayor]++;
+            }
+
+            imprimir(partidos);
+
+            Console.ReadLine();
+        }
+
+        public static void imprimir(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write(m[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void imprimir(int[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine(v[i]);
+            }
+        }
+
+        //devuelve la fila del partido mayor
+        public static int encontrarMayor(int[,] m)
+        {
+            int mayor = m[0, 0];
+            int fila = 0;
+            int col = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    if (m[i, j] > mayor)
+                    {
+                        mayor = m[i, j];
+                        fila = i;
+                        col = j;
+                    }
+                }
+            }
+
+            m[fila, col] = 0;
+            return fila;
+
+        }
+
+
+    }
+
+
+}
--- PruebasDHondt/PruebasDHondt/Program.cs
+++ PruebasDHondt/PruebasDHondt/Program.cs
@@ -1,63 +1,161 @@
-﻿using System.Collections.Immutable;
-using System.Numerics;
+﻿int escañosMadrid = 37;
+int habitantesMadrid = 6921267;
+int habitantesVotaron = 4345230;
+int votosNulos = habitantesVotaron / 20;
 
-int[] partido1 = { 1, 2, 3, 4, 5, 6 };
-int votos1 = 0;
-int[] partido2 = { 7, 8, 9, 10, 11, 12 };
-int votos2 = 0;
-int[] partido3 = { 13, 14, 15, 16, 17, 18 };
-int votos3 = 0;
+int votosValidos = habitantesVotaron - votosNulos;
 
-List<int> lista = new List<int>();
-
-for (int i=0; i<6; i++)
+double[] porcentajes = { 35.25, 24.75, 15.75, 14.25, 3.75, 3.25, 1.5, 0.5, 0.25, 0.25 };
+int cuenta3 = 0;
+for (int i = 0; i < porcentajes.Length; i++)
 {
-    lista.Add(partido1[i]);
-    lista.Add(partido2[i]);
-    lista.Add(partido3[i]);
+    if (porcentajes[i] < 3)
+    {
+        porcentajes[i] = 0;
+    }
 }
 
-int max;
+int[] votos1 = new int[37];
+votos1[0] = (int)((porcentajes[0] * votosValidos) / 100);
 
+int[] votos2 = new int[37];
+votos2[0] = (int)((porcentajes[1] * votosValidos) / 100);
 
-for (int i=0; i<3; i++)
+int[] votos3 = new int[37];
+votos3[0] = (int)((porcentajes[2] * votosValidos) / 100);
+
+int[] votos4 = new int[37];
+votos4[0] = (int)((porcentajes[3] * votosValidos) / 100);
+
+int[] votos5 = new int[37];
+votos5[0] = (int)((porcentajes[4] * votosValidos) / 100);
+
+int[] votos6 = new int[37];
+votos6[0] = (int)((porcentajes[5] * votosValidos) / 100);
+
+int[] votos7 = new int[37];
+votos7[0] = (int)((porcentajes[6] * votosValidos) / 100);
+
+int[] votos8 = new int[37];
+votos8[0] = (int)((porcentajes[7] * votosValidos) / 100);
+
+int[] votos9 = new int[37];
+votos9[0] = (int)((porcentajes[8] * votosValidos) / 100);
+
+int[] votos10 = new int[37];
+votos10[0] = (int)((porcentajes[9] * votosValidos) / 100);
+
+for (int i = 1; i < 37; i++)
 {
-    List<int> listaCopia = new List<int>(lista);
+    votos1[i] = votos1[0] / (i + 1);
+    votos2[i] = votos2[0] / (i + 1);
+    votos3[i] = votos3[0] / (i + 1);
+    votos4[i] = votos4[0] / (i + 1);
+    votos5[i] = votos5[0] / (i + 1);
+    votos6[i] = votos6[0] / (i + 1);
+    votos7[i] = votos7[0] / (i + 1);
+    votos8[i] = votos8[0] / (i + 1);
+    votos9[i] = votos9[0] / (i + 1);
+    votos10[i] = votos10[0] / (i + 1);
+}
+
+List<int> todosVotos = new List<int>();
+for (int i = 0; i < 37; i++)
+{
+    todosVotos.Add(votos1[i]);
+    todosVotos.Add(votos2[i]);
+    todosVotos.Add(votos3[i]);
+    todosVotos.Add(votos4[i]);
+    todosVotos.Add(votos5[i]);
+    todosVotos.Add(votos6[i]);
+    todosVotos.Add(votos7[i]);
+    todosVotos.Add(votos8[i]);
+    todosVotos.Add(votos9[i]);
+    todosVotos.Add(votos10[i]);
+}
+
+List<int> todosVotosCopia = new List<int>(todosVotos);
+
+int escaños1 = 0;
+int escaños2 = 0;
+int escaños3 = 0;
+int escaños4 = 0;
+int escaños5 = 0;
+int escaños6 = 0;
+int escaños7 = 0;
+int escaños8 = 0;
+int escaños9 = 0;
+int escaños10 = 0;
+
+int max = 0;
+
+for (int e = 0; e < escañosMadrid; e++)
+{
+    foreach (int z in todosVotos)
+    {
+        if (z > max)
+        {
+            max = z;
+        }
+    }
+
+    todosVotosCopia.Remove(max);
+    todosVotos = todosVotosCopia;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (max == votos1[i])
+        {
+            escaños1++;
+        }
+        else if (max == votos2[i])
+        {
+            escaños2++;
+        }
+        else if (max == votos3[i])
+        {
+            escaños3++;
+        }
+        else if (max == votos4[i])
+        {
+            escaños4++;
+        }
+        else if (max == votos5[i])
+        {
+            escaños5++;
+        }
+        else if (max == votos6[i])
+        {
+            escaños6++;
+        }
+        else if (max == votos7[i])
+        {
+            escaños7++;
+        }
+        else if (max == votos8[i])
+        {
+            escaños8++;
+        }
+        else if (max == votos9[i])
+        {
+            escaños9++;
+        }
+        else if (max == votos10[i])
+        {
+            escaños10++;
+        }
+    }
+
     max = 0;
-    
-    foreach (int e in listaCopia)
-    {
-        if (e > max)
-        {
-            max = e;
-            
-        }
-    }
-    lista.Remove(max);
-    Console.WriteLine(max);
-
-
-
-
-    for (int j = 0; j < 6; j++)
-    {
-        if (partido1[j] == max)
-        {
-            votos1++;
-        }
-        else if ((partido2[j] == max))
-        {
-            votos2++;
-        }
-        else if ((partido3[j] == max))
-        {
-            votos3++;
-        }
-    }
 }
 
-
-Console.WriteLine(votos1);
-Console.WriteLine(votos2);
-Console.WriteLine(votos3);
-
+Console.WriteLine(escaños1);
+Console.WriteLine(escaños2);
+Console.WriteLine(escaños3);
+Console.WriteLine(escaños4);
+Console.WriteLine(escaños5);
+Console.WriteLine(escaños6);
+Console.WriteLine(escaños7);
+Console.WriteLine(escaños8);
+Console.WriteLine(escaños9);
+Console.WriteLine(escaños10);
